fed0979b10daf8f4dfc075c585e7362ea6538e61
alex100588 <alexandru_ionasc@yahoo.com>
2025-01-14 17:16:16 +0200

    clock

--- projects/02 - JS and CSS Clock/script.js
+++ projects/02 - JS and CSS Clock/script.js
@@ -1,4 +1,7 @@
 const secondHand = document.querySelector('.second-hand')
+const minHand = document.querySelector('.min-hand')
+const hourHand = document.querySelector('.hour-hand')
+
 
 const setDate = () =>{
     const now = new Date()
@@ -7,6 +10,14 @@ const setDate = () =>{
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`
     console.log(seconds);
     
+
+    const mins = now.getMinutes()
+    const minutesDegrees = ((mins / 60) * 360) + 90
+    minHand.style.transform = `rotate(${minutesDegrees}deg)`
+
+    const hour = now.getMinutes()
+    const hoursDegrees = ((mins / 12) * 360) + 90
+    hourHand.style.transform = `rotate(${hoursDegrees}deg)` 
     
 }
 
